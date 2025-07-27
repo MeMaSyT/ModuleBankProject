c497ed7e2f55bc742baaa0923373e46bb7cb1809
MeMaSyT <memasyt228@gmail.com>
2025-07-27 12:41:59 +0300

    удалил ненужный интерфейс

--- ModulebankProject/Features/Transactions/ITransactionsService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ModulebankProject.Features.Transactions;
-
-public interface ITransactionsService
-{
-    Task<Transaction> RegisterTransaction(RegisterTransactionDto dto);
-    Task<(TransactionStatus Status, string Description)> TransferTransaction(Guid id);
-    Task<Transaction?> GetTransaction(Guid id);
-}
